3389530cf8dad70adeb508c1453399d841fd8abc
ning <qiafan666@gmail.com>
2024-05-14 17:02:39 +0800

    config path

--- config/config.go
+++ config/config.go
@@ -31,9 +31,9 @@ func init() {
 	}
 	if len(SC.SConfigure.Profile) == 0 {
 		// load dev profile application-dev.yaml
-		Configs = InitAllConfig(strings.TrimRight(SC.SConfigure.ConfigPath, "\\") + "\\" + "dev")
+		Configs = InitAllConfig(strings.TrimRight(SC.SConfigure.ConfigPath, "/") + "/" + "dev")
 	} else {
-		Configs = InitAllConfig(strings.TrimRight(SC.SConfigure.ConfigPath, "\\") + "\\" + SC.SConfigure.Profile)
+		Configs = InitAllConfig(strings.TrimRight(SC.SConfigure.ConfigPath, "/") + "/" + SC.SConfigure.Profile)
 	}
 }
 
@@ -110,7 +110,7 @@ func InitAllConfig(fileName string) Config {
 	for _, v := range dir {
 		if v.IsDir() == false {
 			if strings.Contains(v.Name(), ".yaml") {
-				file, err := os.ReadFile(fileName + "\\" + v.Name())
+				file, err := os.ReadFile(fileName + "/" + v.Name())
 				if err != nil {
 					panic("load config error")
 				}
