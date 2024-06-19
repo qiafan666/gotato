b20faa5e20e2c802a23f8ab62c2bca4f17c298b9
ning <qiafan666@gmail.com>
2024-06-19 20:44:35 +0800

    add Paginate method

--- commons/utils/function.go
+++ commons/utils/function.go
@@ -10,6 +10,7 @@ import (
 	"github.com/qiafan666/gotato/commons"
 	"github.com/qiafan666/gotato/commons/log"
 	uuid "github.com/satori/go.uuid"
+	"gorm.io/gorm"
 	"gorm.io/gorm/utils"
 	"math/rand"
 	"reflect"
@@ -328,4 +329,21 @@ func DataCheck(datas ...string) []int {
 	}
 
 	return nullIndices
+}
+
+// Paginate 分页
+func Paginate(pageNum int, pageSize int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		if pageNum == 0 {
+			pageNum = 1
+		}
+		switch {
+		case pageSize > 100:
+			pageSize = 100
+		case pageSize <= 0:
+			pageSize = 8
+		}
+		offset := (pageNum - 1) * pageSize
+		return db.Offset(offset).Limit(pageSize)
+	}
 }
