db1a582a24905b9ce8fc615a0b9f769b75325bed
ning <qiafan666@gmail.com>
2024-08-01 21:00:34 +0800

    增加map通用方法

--- commons/gmap/function.go
+++ commons/gmap/function.go
@@ -21,6 +21,27 @@ func MergeMaps[K comparable, V any](maps ...map[K]V) map[K]V {
 	return mergedMap
 }
 
+// MergeMapsUnique 函数用于合并多个同属性的 map, 并确保每个 key 只出现一次（保留第一个出现的键值对）
+func MergeMapsUnique[K comparable, V any](maps ...map[K]V) map[K]V {
+	// 参数校验
+	if len(maps) == 0 {
+		return nil
+	}
+
+	mergedMap := make(map[K]V)
+	for _, m := range maps {
+		if m == nil {
+			continue // 跳过 nil map
+		}
+		for k, v := range m {
+			if _, exists := mergedMap[k]; !exists {
+				mergedMap[k] = v
+			}
+		}
+	}
+	return mergedMap
+}
+
 // MergeMapsConcurrent 并发安全的合并多个同属性的 map
 func MergeMapsConcurrent[K comparable, V any](maps ...map[K]V) map[K]V {
 	// 参数校验
