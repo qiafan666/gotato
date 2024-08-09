0a26d9f9f4bd65ca92a886ae2e371beadb959443
ning <qiafan666@gmail.com>
2024-08-09 11:55:53 +0800

    通用方法

--- commons/gcommon/function.go
+++ commons/gcommon/function.go
@@ -40,6 +40,7 @@ func VersionCompare(rangeVer, curVer string) (bool, error) {
 }
 
 // StructToMap 筛选出非nil的字段，转换成map,跳过指定字段，json标签为空的字段，json标签为数据库字段
+// JumpString 跳过指定字段 不解析第二层struct
 func StructToMap(inputStruct interface{}, JumpString ...string) map[string]interface{} {
 
 	structValue := reflect.ValueOf(inputStruct)
