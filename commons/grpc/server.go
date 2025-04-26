cf77dc0c59ebabc6669956ac30de39a0aa216304
tom <qiafan666@gmail.com>
2025-04-26 17:04:48 +0800

    refactor(slice):修改部分方法名

--- commons/grpc/server.go
+++ commons/grpc/server.go
@@ -1,19 +1,5 @@
 package grpc
 
-import (
-	"context"
-)
-
-type IServer interface {
-	Run(context.Context)
-}
-
 type IHandler interface {
 	Handle(request *Message, ch chan<- *Message)
-}
-
-// SyncHandler 同步处理
-type SyncHandler interface {
-	IHandler
-	Run(context.Context)
 }
